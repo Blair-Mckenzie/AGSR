2b621d8c237b65a15933502afe370656fa16d79a
Blair-Mckenzie <wdb17134@uni.strath.ac.uk>
2022-02-17 21:22:52 +0000

    Adding and editing goals now has validation, created method for showing popup dialog to remove duplication, can now create goals with the same name

--- app/src/main/java/com/example/agsr/fragment_targets.java
+++ app/src/main/java/com/example/agsr/fragment_targets.java
@@ -64,28 +64,7 @@ public class fragment_targets extends Fragment {
         addGoalButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                builder.setTitle("Add New Goal");
-                View viewInflated = LayoutInflater.from(getContext()).inflate(R.layout.add_target_popup, (ViewGroup) getView(), false);
-                EditText titleInput = viewInflated.findViewById(R.id.target_title_input);
-                EditText stepsInput = viewInflated.findViewById(R.id.target_steps_input);
-
-                builder.setView(viewInflated);
-                builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        int numSteps = Integer.parseInt(stepsInput.getText().toString());
-                        targetViewModel.insert(new Target(titleInput.getText().toString(), numSteps, false));
-                    }
-                });
-                builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-                builder.show();
+                showPopupDialog("Add New Goal", 'a', view, 0);
             }
         });
 
@@ -94,42 +73,77 @@ public class fragment_targets extends Fragment {
             public void onDeleteClick(int position) {
                 targetViewModel.delete(adapter.getCurrentList().get(position));
             }
+
             @Override
-            public void onEditClick(int position){
-                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                builder.setTitle("Edit Goal");
-                View viewInflated = LayoutInflater.from(getContext()).inflate(R.layout.add_target_popup, (ViewGroup) getView(), false);
-                EditText titleInput = viewInflated.findViewById(R.id.target_title_input);
-                EditText stepsInput = viewInflated.findViewById(R.id.target_steps_input);
-
-                TextView currentTitle = view.findViewById(R.id.target_title);
-                TextView currentSteps = view.findViewById(R.id.target_steps);
-
-                titleInput.setText(currentTitle.getText());
-                String currentStepsDisplay = currentSteps.getText().toString();
-                stepsInput.setText(currentStepsDisplay.substring(0,currentStepsDisplay.length()-6));
-                builder.setView(viewInflated);
-
-                builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        Target updatedTarget = adapter.getCurrentList().get(position);
-                        updatedTarget.setTitle(titleInput.getText().toString());
-                        updatedTarget.setNumSteps(Integer.parseInt(stepsInput.getText().toString()));
-                        targetViewModel.update(updatedTarget);
-                    }
-                });
-                builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-                builder.show();
+            public void onEditClick(int position) {
+                showPopupDialog("Edit Goal", 'e', view, position);
             }
         });
         return view;
     }
 
+    private boolean checkDialogEmpty(EditText title, EditText numSteps) {
+        boolean err = false;
+        if (title.getText().toString().length() == 0 && numSteps.getText().toString().length() == 0) {
+            title.setError("Required");
+            numSteps.setError("Required");
+            err = true;
+        } else if (title.getText().toString().length() == 0) {
+            title.setError("Required");
+            err = true;
+        } else if (numSteps.getText().toString().length() == 0) {
+            numSteps.setError("Required");
+            err = true;
+        }
+        return err;
+    }
+
+    private void showPopupDialog(String title, char type, View view, int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle(title);
+        View viewInflated = LayoutInflater.from(getContext()).inflate(R.layout.add_target_popup, (ViewGroup) getView(), false);
+        EditText titleInput = viewInflated.findViewById(R.id.target_title_input);
+        EditText stepsInput = viewInflated.findViewById(R.id.target_steps_input);
+        if (type == 'e') {
+            TextView currentTitle = view.findViewById(R.id.target_title);
+            TextView currentSteps = view.findViewById(R.id.target_steps);
+
+            titleInput.setText(currentTitle.getText());
+            String currentStepsDisplay = currentSteps.getText().toString();
+            stepsInput.setText(currentStepsDisplay.substring(0, currentStepsDisplay.length() - 6));
+            builder.setView(viewInflated);
+        }
+        builder.setView(viewInflated);
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        final AlertDialog dialog = builder.create();
+        dialog.show();
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!(checkDialogEmpty(titleInput, stepsInput))) {
+                    dialog.dismiss();
+                    if (type == 'a') {
+                        int numSteps = Integer.parseInt(stepsInput.getText().toString());
+                        targetViewModel.insert(new Target(titleInput.getText().toString(), numSteps, false));
+                    } else {
+                        Target updatedTarget = adapter.getCurrentList().get(position);
+                        updatedTarget.setTitle(titleInput.getText().toString());
+                        updatedTarget.setNumSteps(Integer.parseInt(stepsInput.getText().toString()));
+                        targetViewModel.update(updatedTarget);
+                    }
+
+                }
+            }
+        });
+    }
 }
